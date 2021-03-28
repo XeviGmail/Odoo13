bd65092ed939e6a4b1ec0d7ad68a089d2b2c69cf
x.mesones@gmail.com <(G1)/i2=60>
2021-03-28 10:12:19 +0200

    Creamos boton para resetar el contador desde Backend con Javascript

--- Learning_FrontBackend/static/src/js/field.js
+++ Learning_FrontBackend/static/src/js/field.js
@@ -6,6 +6,19 @@ odoo.define('Learning_FrontBackend.field', (require) => {
     const FieldForShare = Widget.extend({
         // Busca un ID externo para renderizar la vista asociada al ID externo
         template: 'Learning_FrontBackend.share_button',
+        events: {
+            'click .js_reset_field': 'resetShareCount'
+        },
+
+//        async resetShareCount(ev){
+//            await this._setValue('0');
+//            this.renderElement();
+//        },
+
+        resetShareCount(){
+            this._setValue('0');
+        },
+
 
         // isSet evalua cuando hay que mostrar el widget
         isSet(){
