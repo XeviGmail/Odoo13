75102a8594cbb04924396f8c69b8f7e9a94c6d8b
Xevi <x.mesones@gmail.com>
2024-11-14 23:34:38 +0100

    first commit

---
+++ Personal/leaflet_map/static/src/lmap_view.js
@@ -0,0 +1,18 @@
+//* @odoo-module */
+
+import { registry } from "@web/core/registry"
+import { Component, xml } from "@odoo/owl"
+
+class LeafletMapController extends Component{
+    static template = xml`<div>Leaflet Map View</div>`;
+}
+
+const leafletMapView = {
+    type: "lmap",
+    display_name: "Leaflet Map",
+    icon: "fa fa-map-marker",
+    multiRecord: true,
+    Controller:LeafletMapController,
+}
+
+registry.category("views").add("lmap", leafletMapView)
--- Personal/xmscript/__manifest__.py
+++ Personal/xmscript/__manifest__.py
@@ -2,8 +2,9 @@
     "version": "1.0",
     "name": "XMScript Media",
     "description": "XMScript Mesdia",
-    "data": {
+    "depends": ["leaflet_map"],
+    "data": [
         "security/ir.model.access.csv",
         "views/contact.xml",
-    }
+    ]
 }
